cce57064e484971396d83e7e6fa9f4073f2cdd4d
douglasjsmith404 <djs404@proton.me>
2024-04-18 11:48:38 -0400

    changed out for static adaptor

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,13 @@
-// import adapter from '@sveltejs/adapter-auto'; 
-import adapter from '@sveltejs/adapter-node';
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-//	kit: {
-//		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-//		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-//		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-//		adapter: adapter()
-//	}
-// };
+import adapter from '@sveltejs/adapter-static';
 
-// export default config;
-
-export default {
+const config = {
   kit: {
-    adapter: adapter()
-  }
+    adapter: adapter(),
+    paths: {
+      base: '/douglasjsmith404.github.io', // replace 'repo-name' with your repository name
+      assets: '/douglasjsmith404.github.io', // replace 'repo-name' with your repository name
+    },
+  },
 };
+
+export default config;
