2f796f8a72bb10a96a1d43a2ea8a6dfe526956fc
douglasjsmith404 <djs404@proton.me>
2024-04-19 08:56:31 -0400

    rebuild

--- build/_app/immutable/entry/start.BujWf1wV.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.C-cXW92V.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	kit: {
 		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: '',
-      assets: ''
-		}
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
 	}
 };
-
-export default config;
